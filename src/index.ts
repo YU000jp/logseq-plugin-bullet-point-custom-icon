17ee1258f3423c74f9e9d59a9ee99b5ed8bd24ca
YU <111847207+YU000jp@users.noreply.github.com>
2023-10-14 08:16:34 +0900

    feat(init):
    プラグイン設定を見やすくする
    設定項目の追加(アイコンサイズの拡大、)
    タグの大文字小文字の区別をはずす(Logseqは小文字ベース)

--- src/index.ts
+++ src/index.ts
@@ -24,14 +24,28 @@ const main = () => {
 
   //常時適用CSS
   logseq.provideStyle(`
-  div#root>div>main>div#app-container a.tag[data-ref*=".side"] {display: none}
+  article>div[data-id="bullet-point-custom-icon"] div.heading-item {
+    margin-top: 2em;
+    border-top-width: 1px;
+    padding-top: 1em;
+  }
+  article>div[data-id="bullet-point-custom-icon"] label.form-control{
+    &>input[type="text"].form-input {
+      width:100px;
+      font-size: 1.3em;
+    }
+    &>textarea.form-input {
+      width:350px;
+      height: 9em;
+    }
+  }
   `);
 
 
   //ツールバーに設定画面を開くボタンを追加
   logseq.App.registerUIItem('toolbar', {
     key: 'customBulletIconSettingsButton',
-    template: `<div><a class="button icon" data-on-click="customBulletIconSettingsButton" style="font-size: 14px">🥦</a></div>`,
+    template: `<div><a class="button icon" data-on-click="customBulletIconSettingsButton" style="font-size: 14px">🌞</a></div>`,
   });
   //ツールバーボタンのクリックイベント
   logseq.provideModel({
@@ -44,15 +58,147 @@ const main = () => {
   logseq.onSettingsChanged((newSet: LSPluginBaseInfo['settings'], oldSet: LSPluginBaseInfo['settings']) => {
     if (newSet.booleanFunction !== oldSet.booleanFunction) {
       if (newSet.booleanFunction === true) provideStyle();
-      else removeProvideStyle("bullet-point-custom-icon");
-    }
+      else
+        if (newSet.booleanFunction === false) removeProvideStyle("bullet-point-custom-icon");
+    } else
+      if (oldSet.booleanAtMarkTagHidden !== newSet.booleanAtMarkTagHidden
+        || oldSet.booleanHierarchyParentTag !== newSet.booleanHierarchyParentTag
+        || oldSet.booleanIconLarge !== newSet.booleanIconLarge
+        || oldSet.icon01 !== newSet.icon01
+        || oldSet.icon02 !== newSet.icon02
+        || oldSet.icon03 !== newSet.icon03
+        || oldSet.icon04 !== newSet.icon04
+        || oldSet.icon05 !== newSet.icon05
+        || oldSet.icon06 !== newSet.icon06
+        || oldSet.icon07 !== newSet.icon07
+        || oldSet.icon08 !== newSet.icon08
+        || oldSet.icon09 !== newSet.icon09
+        || oldSet.icon10 !== newSet.icon10
+        || oldSet.icon11 !== newSet.icon11
+        || oldSet.icon12 !== newSet.icon12
+        || oldSet.tagsList01 !== newSet.tagsList01
+        || oldSet.tagsList02 !== newSet.tagsList02
+        || oldSet.tagsList03 !== newSet.tagsList03
+        || oldSet.tagsList04 !== newSet.tagsList04
+        || oldSet.tagsList05 !== newSet.tagsList05
+        || oldSet.tagsList06 !== newSet.tagsList06
+        || oldSet.tagsList07 !== newSet.tagsList07
+        || oldSet.tagsList08 !== newSet.tagsList08
+        || oldSet.tagsList09 !== newSet.tagsList09
+        || oldSet.tagsList10 !== newSet.tagsList10
+        || oldSet.tagsList11 !== newSet.tagsList11
+        || oldSet.tagsList12 !== newSet.tagsList12
+        || oldSet.colorBoolean01 !== newSet.colorBoolean01
+        || oldSet.colorBoolean02 !== newSet.colorBoolean02
+        || oldSet.colorBoolean03 !== newSet.colorBoolean03
+        || oldSet.colorBoolean04 !== newSet.colorBoolean04
+        || oldSet.colorBoolean05 !== newSet.colorBoolean05
+        || oldSet.colorBoolean06 !== newSet.colorBoolean06
+        || oldSet.colorBoolean07 !== newSet.colorBoolean07
+        || oldSet.colorBoolean08 !== newSet.colorBoolean08
+        || oldSet.colorBoolean09 !== newSet.colorBoolean09
+        || oldSet.colorBoolean10 !== newSet.colorBoolean10
+        || oldSet.colorBoolean11 !== newSet.colorBoolean11
+        || oldSet.colorBoolean12 !== newSet.colorBoolean12
+        || oldSet.color01 !== newSet.color01
+        || oldSet.color02 !== newSet.color02
+        || oldSet.color03 !== newSet.color03
+        || oldSet.color04 !== newSet.color04
+        || oldSet.color05 !== newSet.color05
+        || oldSet.color06 !== newSet.color06
+        || oldSet.color07 !== newSet.color07
+        || oldSet.color08 !== newSet.color08
+        || oldSet.color09 !== newSet.color09
+        || oldSet.color10 !== newSet.color10
+        || oldSet.color11 !== newSet.color11
+        || oldSet.color12 !== newSet.color12
+      ) reset();
   });
+
 };/* end_main */
 
+const reset = () => {
+  removeProvideStyle("bullet-point-custom-icon");
+  provideStyle();
+};
 
-const provideStyle = () => logseq.provideStyle({ key: 'bullet-point-custom-icon', style: CSS });
+
+const provideStyle = () => {
+  if (logseq.settings!.booleanFunction === false) return;
+
+  let printCSS = "";
+
+  //サンプル
+  // &[data-refs-self*='"@page']>.flex.flex-row.pr-2 .bullet-container .bullet:before {
+  //     content: "\eaa4" !important;
+  //     color: red;
+  // }
+
+  //設定に合わせて生成するCSS
+
+  //12個複製する
+  let tagsListFull: string[] = [];
+  for (let i = 1; i < 13; i++) {
+    //二桁にしたい
+    const count = ("0" + i).slice(-2);
+    const { outCSS, outTagsList } = eachCreateCSS(count);
+    tagsListFull = tagsListFull.concat(outTagsList);
+    printCSS += outCSS;
+  }
+
+  if (printCSS !== "" && tagsListFull.length > 0) {
+    //動的CSS
+    logseq.provideStyle({
+      key: 'bullet-point-custom-icon', style: `
+    div#root>div>main>div#app-container {
+      ${logseq.settings!.booleanAtMarkTagHidden === true ? `
+      /*Optional: hide tags with @*/
+      & a.tag[data-ref*="@"] {
+          display: none;
+      }
+  `: ""}
+      & div.ls-block {
+          &:is([data-refs-self*='"@'],${[...new Set(tagsListFull)].map((tag) => `${logseq.settings!.booleanHierarchyParentTag === false ? `:not([data-refs-self*='${tag}/' i])` : ""}[data-refs-self*='"${tag}"' i]`).join(",")}) {
+            ${logseq.settings!.booleanIconLarge === "large" ?
+          "&>.flex.flex-row.pr-2 .bullet-container .bullet:before {font-size: 1.2em;}"
+          : logseq.settings!.booleanIconLarge === "x-large" ? "&>.flex.flex-row.pr-2 .bullet-container .bullet:before {font-size: 1.5em;}"
+            : ""}  
+            ${CSS}
+          }
+      }
+    }
+    `
+        + "\ndiv#root>div>main>div#app-container div.ls-block {\n" + printCSS + "\n}\n"
+    });
+  } else {
+    //固定CSS
+    if (logseq.settings!.booleanAtMarkTagHidden === true) logseq.provideStyle({
+      key: 'bullet-point-custom-icon', style: `
+    div#root>div>main>div#app-container {
+      /*Optional: hide tags with @*/
+      & a.tag[data-ref*="@"] {
+          display: none;
+      }
+    }
+    ` });
+  }
+
+};
 
 
+const eachCreateCSS = (count: string) => {
+  let outCSS = "";
+  //各行が空でないこと
+  const outTagsList: string[] = logseq.settings![`tagsList${count}`].split("\n").filter((tag) => tag !== "");
+  if (logseq.settings![`icon${count}`] !== "" || outTagsList.length > 0) {
+    outCSS = `&:is(${outTagsList.map((tag) => `${logseq.settings!.booleanHierarchyParentTag === false ? `:not([data-refs-self*='${tag}/' i])` : ""}[data-refs-self*='"${tag}"' i]`).join(",")})>.flex.flex-row.pr-2 .bullet-container .bullet:before {
+        content: "${logseq.settings![`icon${count}`]}" !important;
+        ${logseq.settings![`colorBoolean${count}`] === true ? `color: ${logseq.settings![`color${count}`]};` : ""}
+      }
+      `;
+  }
+  return { outCSS, outTagsList };
+};
 
 
 logseq.ready(main).catch(console.error);
--- src/settings.ts
+++ src/settings.ts
@@ -1,20 +1,106 @@
 import { SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin.user';
+import { t } from 'logseq-l10n';
 
 /* user setting */
 // https://logseq.github.io/plugins/types/SettingSchemaDesc.html
-export const settingsTemplate = (): SettingSchemaDesc[] => [
-    {
-        key: "booleanFunction",
-        type: "boolean",
-        title: "On/Off",
-        default: true,
-        description: "",
-    },
-    {
-        key: "heading001",
-        type: "heading",
-        title: "no settings",
-        default: "",
-        description: "",
-    },
-];
+export const settingsTemplate = (): SettingSchemaDesc[] => {
+    const settingArray: SettingSchemaDesc[] = [
+        {//機能の一時的なオンオフ
+            key: "booleanFunction",
+            type: "boolean",
+            title: t("Function On/Off"),
+            default: true,
+            description: "",
+        },
+        {//@を含むタグがついている場合、編集中以外は非表示
+            key: "booleanAtMarkTagHidden",
+            type: "boolean",
+            title: t("Hide the tag of including `@` ? (except when editing)"),
+            default: true,
+            description: "default: true",
+        },
+        {//指定したタグにHierarchyが含まれている場合、その親に一致する場合にもマッチさせる
+            key: "booleanHierarchyParentTag",
+            type: "boolean",
+            title: t("Match parent tag? (if the specified tag contains Hierarchy)"),
+            default: false,
+            description: "default: false",
+        },
+        {//アイコンの大きさを大きくする
+            key: "booleanIconLarge",
+            type: "enum",
+            title: t("Large icon?"),
+            enumChoices: ["default", "large", "x-large"],
+            default: "default",
+            description: "",
+        },
+        {//絵文字コピーサイトへの誘導リンク
+            key: "headingEmojiCopy",
+            type: "heading",
+            title: t("Use Emoji icon"),
+            default: "",
+            description: t("Press the shortcut key and enter from the selection screen, or copy emoji on the site to clipboard. ") + "https://emojilo.com/ ",
+        },
+        {//Tabler iconの場合は、プラグインをインストールして、アイコンをコピーする
+            key: "headingTablerIconCopy",
+            type: "heading",
+            title: t("Use Tabler icon"),
+            default: "",
+            description: t("Install `Tabler-icon` plugin. Then copy icons to clipboard from toolbar."),
+        }
+    ];
+
+    //option
+
+    //12種類のアイコンを設定する
+    const iconArray = ["🌟", "👍", "🔵", "📚", "📌", "📝", "📖", "❓", "🌳", "🐶", "🚗", "🔥"];
+
+    //12個複製する
+    for (let i = 0; i < 12; i++) {
+        //二桁にしたい
+        const count = ("0" + (i + 1)).slice(-2);
+        settingArray.push(
+            {
+                key: `heading${count}`,
+                type: "heading",
+                title: `No. ${count}`,
+                default: "",
+                description: t("Bullets in blocks with that tag are effected."),
+            },
+            {
+                key: `icon${count}`,
+                type: "string",
+                title: t("Tabler-icon or Emoji icon (`Win + .` / Mac: `cmd + ctrl + space`)"),
+                //一文字のみ
+                description: t("one character(mark) only"),
+                default: iconArray[i],
+            },
+            {
+                key: `tagsList${count}`,
+                type: "string",
+                inputAs: "textarea",
+                title: t("Specify one or more tags"),
+                description: t("separated by line breaks"),
+                default: "",
+            },
+            {
+                key: `colorBoolean${count}`,
+                type: "boolean",
+                title: t("Colorize?"),
+                //Tabler-iconの場合のみ
+                description: t("Tabler-icon only"),
+                default: false,
+            },
+            {
+                key: `color${count}`,
+                type: "string",
+                inputAs: "color",
+                title: t("Specify the color of the icon"),
+                description: t("Tabler-icon only"),
+                default: "#6032A4",
+            },
+        );
+    }
+
+    return settingArray;
+};
