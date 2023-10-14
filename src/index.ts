66465a4e70da3d15f90517fa5755d53c60d5443b
YU <111847207+YU000jp@users.noreply.github.com>
2023-10-14 17:31:56 +0900

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import '@logseq/libs'; //https://plugins-doc.logseq.com/
-import { settingsTemplate } from './settings';
+import { settingsTemplate, settingChanged } from './settings';
 import { setup as l10nSetup, t } from "logseq-l10n"; //https://github.com/sethyuan/logseq-l10n
 import ja from "./translations/ja.json";
-import { removeProvideStyle } from './lib';
 import { BlockEntity } from '@logseq/libs/dist/LSPlugin.user';
 import CSS from './style.css?inline';
-import { settingChanged } from './settings';
+import { tableIconNonUnicode, tablerIconGetUnicode } from './lib';
+export const keyNameToolbarPopup = "toolbar-box";//ポップアップのキー名
 
 /* main */
 const main = () => {
@@ -14,7 +14,7 @@ const main = () => {
       await l10nSetup({ builtinTranslations: { ja } });
     } finally {
       /* user settings */
-      logseq.useSettingsSchema(settingsTemplate());
+      logseq.useSettingsSchema(await settingsTemplate());
       if (!logseq.settings) setTimeout(() => logseq.showSettingsUI(), 300);
     }
   })();
@@ -49,91 +49,11 @@ const main = () => {
   //ツールバーに設定画面を開くボタンを追加
   logseq.App.registerUIItem('toolbar', {
     key: 'customBulletIconToolbar',
-    template: `<div id="customBulletIconSettingsButton" data-rect><a class="button icon" data-on-click="customBulletIconToolbar" style="font-size: 14px" title="Bullet Point Custom Icon: plugin settings">#️⃣</a></div>`,
+    template: `<div id="customBulletIconSettingsButton" data-rect><a class="button icon" data-on-click="customBulletIconToolbar" style="font-size: 15px;color:#1f9ee1" title="Bullet Point Custom Icon: plugin settings">#️⃣</a></div>`,
   });
   //ツールバーボタンのクリックイベント
   logseq.provideModel({
-    customBulletIconToolbar: () => {
-      let printCurrentSettings = `
-      <div>
-      <h1>${t("Current settings")} <button class="button" id="showSettingsUI">⚙️</button></h1>
-      <p>${t("Select a block first, then click the tag name to insert that tag.")}</p>
-      <hr/>
-      `;
-      //現在の設定を表示
-      let tagsListFull: string[] = [];
-      for (let i = 1; i < 13; i++) {
-        //二桁にしたい
-        const count = ("0" + i).slice(-2);
-        const { outCSS, outTagsList } = eachCreateCSS(count);
-        if (outTagsList.length > 0) {
-          //タグを一つずつ表示し、<button>でクリックするとそのタグをコピーする。,で区切る
-          const tagsList = outTagsList.map((tag) => `<button class="button" id="customBulletIconToolbar--${count}${tag}">#${tag}</button>`) || "(none)";
-          printCurrentSettings += `
-          <div>${t("Icon")} ${count}: ${logseq.settings![`icon${count}`]}<br/>
-          ${t("Tags")}: <span style="display:flex">${tagsList}</span></div>
-          <hr/>
-          `;
-          //outTagsListに${count}を付けて、タグを一つずつ表示し、<button>でクリックするとそのタグをコピーする。,で区切る
-          outTagsList.forEach((tag) => {
-            setTimeout(() => {
-              const copyTag = parent.document.getElementById(`customBulletIconToolbar--${count}${tag}`) as HTMLButtonElement | null;
-              if (copyTag) copyTag.addEventListener("click", () => copyEvent(tag));
-            }, 120);
-          });
-        }
-      }
-      printCurrentSettings += "</div>";
-
-      //ポップアップを表示
-      logseq.provideUI({
-        attrs: {
-          title: "Bullet Point Custom Icon plugin",
-        },
-        key: 'toolbar-box',
-        reset: true,
-        close: "outside",
-        style: {
-          width: "400px",
-          minHeight: "500px",
-          maxHeight: "1630px",
-          overflowY: "auto",
-          left: "unset",
-          bottom: "unset",
-          right: "1em",
-          top: "4em",
-          paddingLeft: "2em",
-          paddingTop: "2em",
-          backgroundColor: 'var(--ls-primary-background-color)',
-          color: 'var(--ls-primary-text-color)',
-          boxShadow: '1px 2px 5px var(--ls-secondary-background-color)',
-        },
-        template: `
-        <div title="">
-        ${printCurrentSettings}
-        </div>
-        <style>
-        body>div#bullet-point-custom-icon--toolbar-box {
-          & h1 {
-            font-size: 1.5em;
-          }
-          & button {
-            display: unset;
-          }
-          & hr {
-            margin-top: 1em;
-            margin-bottom: 1em;
-          }
-        }
-        </style>
-        `,
-      });
-      setTimeout(() => {
-        //設定画面を開くボタンをクリックしたら、設定画面を開く
-        const showSettingsUI = parent.document.getElementById("showSettingsUI") as HTMLButtonElement | null;
-        if (showSettingsUI) showSettingsUI.addEventListener("click", () => logseq.showSettingsUI(), { once: true });
-      }, 50);
-    },
+    customBulletIconToolbar: () => openPopupFromToolbar(),
     showSettingsUI: () => logseq.showSettingsUI(),
   });
 
@@ -141,11 +61,6 @@ const main = () => {
   settingChanged();
 
 };/* end_main */
-
-export const reset = () => {
-  removeProvideStyle("bullet-point-custom-icon");
-  provideStyle();
-};
 
 
 export const provideStyle = () => {
@@ -184,9 +99,9 @@ export const provideStyle = () => {
   `: ""}
       & div.ls-block {
           &:is(${[...new Set(tagsListFull)].map((tag) => `${logseq.settings!.booleanHierarchyParentTag === false ? `:not([data-refs-self*='${tag}/' i])` : ""}[data-refs-self*='"${tag}"' i]`).join(",")}) {
-            ${logseq.settings!.booleanIconLarge === "large" ?
-          "&>.flex.flex-row.pr-2 .bullet-container .bullet:before {font-size: 1.2em;}"
-          : logseq.settings!.booleanIconLarge === "x-large" ? "&>.flex.flex-row.pr-2 .bullet-container .bullet:before {font-size: 1.5em;}"
+            ${logseq.settings!.booleanIconLarge === "medium" ?
+          "&>.flex.flex-row.pr-2 .bullet-container .bullet:before {font-size: 1.2em;top: -10px!important;left: -7px!important;}"
+          : logseq.settings!.booleanIconLarge === "large" ? "&>.flex.flex-row.pr-2 .bullet-container .bullet:before {font-size: 1.5em;top:-13px!important;left:-8.5px!important;}"
             : ""}  
             ${CSS}
           }
@@ -217,7 +132,7 @@ const eachCreateCSS = (count: string) => {
   const outTagsList: string[] = (logseq.settings![`tagsList${count}`].includes("\n") ? logseq.settings![`tagsList${count}`].split("\n") : [logseq.settings![`tagsList${count}`]]).filter((tag) => tag !== "");
   if (logseq.settings![`icon${count}`] !== "" || outTagsList.length > 0) {
     outCSS = `&:is(${outTagsList.map((tag) => `${logseq.settings!.booleanHierarchyParentTag === false ? `:not([data-refs-self*='${tag}/' i])` : ""}[data-refs-self*='"${tag}"' i]`).join(",")})>.flex.flex-row.pr-2 .bullet-container .bullet:before {
-        content: "${logseq.settings![`icon${count}`]}" !important;
+        content: "${tableIconNonUnicode(count)}" !important;
         ${logseq.settings![`colorBoolean${count}`] === true ? `color: ${logseq.settings![`color${count}`]};` : ""}
       }
       `;
@@ -233,16 +148,109 @@ const copyEvent = async (tag: string) => {
     const ele = parent.document.querySelector(`div#root>div>main>div#app-container div[blockid="${currentBlock.uuid}"]`) as HTMLDivElement | null;
     if (ele) {
       ele.style.outline = "3px solid var(--ls-border-color)";
-      setTimeout(() => ele.style.outline = "unset", 4000);
+      setTimeout(() => ele.style.outline = "unset", 6000);
     }
     //現在のブロックの最後にタグを追加
+    //tagに空白が含まれていたら、[[ ]]で囲む
+    if (tag.includes(" ")) tag = "[[" + tag + "]]";
     await logseq.Editor.updateBlock(currentBlock.uuid, currentBlock.content + " #" + tag, currentBlock.properties);
-    logseq.UI.showMsg("Insert at editing block: #" + tag);
+    logseq.UI.showMsg("Insert at editing block: #" + tag + ".", "info");
     logseq.Editor.editBlock(currentBlock.uuid);
   } else {
     //ブロックが選択されていない場合
     logseq.UI.showMsg("No block selected.", "warning");
   }
+};
+
+export const openPopupSettingsChanged = () => {
+  openPopupFromToolbar();
+};
+
+const openPopupFromToolbar = () => {
+  let printCurrentSettings = `
+      <div>
+      <h1>${t("Current settings")} <button class="button" id="bullet-point-custom-icon--showSettingsUI">⚙️</button></h1>
+      <p>${t("Select a block first, then click the tag name to insert that tag.")}</p>
+      <hr/>
+      `;
+  //現在の設定を表示
+  for (let i = 1; i < 13; i++) {
+    //二桁にしたい
+    const count = ("0" + i).slice(-2);
+    const { outCSS, outTagsList } = eachCreateCSS(count);
+    if (outTagsList.length > 0) {
+      //タグを一つずつ表示し、<button>でクリックするとそのタグをコピーする。,で区切る
+      const tagsList = outTagsList.map((tag) => `<button class="button" id="customBulletIconToolbar--${count}${tag}" title="${t("Click here to insert a tag at the end of the current block.")}">#${tag}</button>`) || "(none)";
+
+      const icon = tablerIconGetUnicode(count);
+      printCurrentSettings += `
+          <div><small>${t("Icon")} ${count}:</small><span class="tabler-icon"${logseq.settings![`colorBoolean${count}`] === true ? ` style="color:${logseq.settings![`color${count}`]}"` : ""}>${icon}</span>
+          <span style="display:unset">${tagsList}</span></div>
+          <hr/>
+          `;
+      //outTagsListに${count}を付けて、タグを一つずつ表示し、<button>でクリックするとそのタグをコピーする。,で区切る
+      outTagsList.forEach((tag) => {
+        setTimeout(() => {
+          const copyTag = parent.document.getElementById(`customBulletIconToolbar--${count}${tag}`) as HTMLButtonElement | null;
+          if (copyTag) copyTag.addEventListener("click", () => copyEvent(tag));
+        }, 120);
+      });
+    }
+  }
+  printCurrentSettings += "</div>";
+
+  //ポップアップを表示
+  logseq.provideUI({
+    attrs: {
+      title: "Bullet Point Custom Icon plugin",
+    },
+    key: keyNameToolbarPopup,
+    reset: true,
+    style: {
+      width: "370px",
+      minHeight: "500px",
+      maxHeight: "94vh",
+      overflowY: "auto",
+      left: "unset",
+      bottom: "unset",
+      right: "1em",
+      top: "4em",
+      paddingLeft: "2em",
+      paddingTop: "2em",
+      backgroundColor: 'var(--ls-primary-background-color)',
+      color: 'var(--ls-primary-text-color)',
+      boxShadow: '1px 2px 5px var(--ls-secondary-background-color)',
+    },
+    template: `
+        <div title="">
+        ${printCurrentSettings}
+        </div>
+        <style>
+        body>div#bullet-point-custom-icon--toolbar-box {
+          & h1 {
+            font-size: 1.5em;
+          }
+          & button {
+            display: unset;
+          }
+          & hr {
+            margin-top: 1em;
+            margin-bottom: 1em;
+          }
+          & span.tabler-icon {
+            font-family: 'tabler-icons';
+            font-size: 1.5em;
+            margin-left: 0.7em;
+          }
+        }
+        </style>
+        `,
+  });
+  setTimeout(() => {
+    //設定画面を開くボタンをクリックしたら、設定画面を開く
+    const showSettingsUI = parent.document.getElementById("bullet-point-custom-icon--showSettingsUI") as HTMLButtonElement | null;
+    if (showSettingsUI) showSettingsUI.addEventListener("click", () => logseq.showSettingsUI(), { once: true });
+  }, 50);
 };
 
 
--- src/lib.ts
+++ src/lib.ts
@@ -1,4 +1,5 @@
-import { provideStyle, reset } from '.';
+import { provideStyle } from '.';
+import { reset } from './settings';
 import { LSPluginBaseInfo } from '@logseq/libs/dist/LSPlugin.user';
 
 export const removeProvideStyle = (className: string) => {
@@ -67,3 +68,13 @@ export const settingChanged = () => {
             || oldSet.color12 !== newSet.color12) reset();
     });
 };
+
+export const tableIconNonUnicode = (count: string) => logseq.settings![`icon${count}`].length === 4 ? "\\" + logseq.settings![`icon${count}`] : logseq.settings![`icon${count}`];
+
+export const tablerIconGetUnicode = (count: string) => logseq.settings![`icon${count}`] ?
+    //4桁の文字列であれば、&#xを付けて、;を付ける
+    logseq.settings![`icon${count}`].length === 4 ? `&#x${logseq.settings![`icon${count}`] as string};` :
+        //5桁の文字列であれば、\を削除して、&#xを付けて、;を付ける
+        logseq.settings![`icon${count}`].length === 5 ?
+            `&#x${(logseq.settings![`icon${count}`] as string).replace("\\", "") as string};` : logseq.settings![`icon${count}`]
+    : "(none)";
