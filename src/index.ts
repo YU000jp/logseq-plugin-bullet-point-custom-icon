87e4ec3ca8427ded23dbeec9be282e368e676f9c
YU <111847207+YU000jp@users.noreply.github.com>
2025-06-08 10:44:47 +0900

    feat(): Compatibility for DB model (file-based graph only) #11

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,53 @@
-import '@logseq/libs'; //https://plugins-doc.logseq.com/
-import { settingsTemplate, settingChanged } from './settings';
-import { setup as l10nSetup, t } from "logseq-l10n"; //https://github.com/sethyuan/logseq-l10n
-import ja from "./translations/ja.json";
-import CSS from './bullet.css?inline';
-import { tableIconNonUnicode, tablerIconGetUnicode } from './lib';
-import { copyEvent } from './lib';
-export const keyNameToolbarPopup = "toolbar-box";//ポップアップのキー名
+import '@logseq/libs' //https://plugins-doc.logseq.com/
+import { settingsTemplate, settingChanged } from './settings'
+import { setup as l10nSetup, t } from "logseq-l10n" //https://github.com/sethyuan/logseq-l10n
+import ja from "./translations/ja.json"
+import CSS from './bullet.css?inline'
+import { tableIconNonUnicode, tablerIconGetUnicode } from './lib'
+import { copyEvent } from './lib'
+import { AppInfo } from '@logseq/libs/dist/LSPlugin.user'
+export const keyNameToolbarPopup = "toolbar-box"//ポップアップのキー名
+let logseqVersion: string = "" //バージョンチェック用
+let logseqVersionMd: boolean = false //バージョンチェック用
+let logseqDbGraph: boolean = false
+// export const getLogseqVersion = () => logseqVersion //バージョンチェック用
+export const booleanLogseqVersionMd = () => logseqVersionMd //バージョンチェック用
+export const booleanDbGraph = () => logseqDbGraph //バージョンチェック用
 
 /* main */
-const main = () => {
-  (async () => {
-    try {
-      await l10nSetup({ builtinTranslations: { ja } });
-    } finally {
-      /* user settings */
-      logseq.useSettingsSchema(await settingsTemplate());
-      if (!logseq.settings) setTimeout(() => logseq.showSettingsUI(), 300);
-    }
-  })();
+const main = async (notFirst?: boolean) => {
+
+  // バージョンチェック
+  if (notFirst !== true)
+    logseqVersionMd = await checkLogseqVersion()
+  // console.log("logseq version: ", logseqVersion)
+  // console.log("logseq version is MD model: ", logseqVersionMd)
+  // 100ms待つ
+  await new Promise(resolve => setTimeout(resolve, 100))
+
+  // if (logseqVersionMd === false) {
+  //   // Logseq ver 0.10.*以下にしか対応していない
+  //   logseq.UI.showMsg("The ’Bullet Point Custom Icon’ plugin only supports Logseq ver 0.10.* and below.", "warning", { timeout: 5000 })
+  //   return
+  // }
+  // // DBグラフチェック
+  logseqDbGraph = await checkLogseqDbGraph()
+  if (logseqDbGraph === true) {
+    // DBグラフには対応していない
+    logseq.UI.showMsg("The ’Bullet Point Custom Icon’ plugin not supports Logseq DB graph.", "warning", { timeout: 5000 })
+    return
+  }
+
+  await l10nSetup({ builtinTranslations: { ja } })
+
+  /* user settings */
+  logseq.useSettingsSchema(await settingsTemplate())
+  if (!logseq.settings) setTimeout(() => logseq.showSettingsUI(), 300)
+
 
   //初回読み込み時にCSSを反映させる
   /* side block */
-  provideStyle();
+  provideStyle()
 
 
   let settingsCSS = ''
@@ -121,31 +147,37 @@ const main = () => {
       }
     }
   }
-  `);
+  `)
 
 
   //ツールバーに設定画面を開くボタンを追加
   logseq.App.registerUIItem('toolbar', {
     key: 'customBulletIconToolbar',
     template: `<div id="customBulletIconSettingsButton" data-rect><a class="button icon" data-on-click="customBulletIconToolbar" style="font-size:15px;color:#1f9ee1;opacity:unset" title="Bullet Point Custom Icon: ${t("plugin settings")}">#️⃣</a></div>`,
-  });
+  })
   //ツールバーボタンのクリックイベント
   logseq.provideModel({
     customBulletIconToolbar: () => openPopupFromToolbar(),
     showSettingsUI: () => logseq.showSettingsUI(),
-  });
+  })
 
   //Setting changed
-  settingChanged();
+  settingChanged()
 
-};/* end_main */
+
+  if (notFirst !== true)
+    logseq.App.onCurrentGraphChanged(async () => {
+      logseqDbGraph = await checkLogseqDbGraph()
+      if (logseqDbGraph === false) main(true)
+    })
+}/* end_main */
 
 
 export const provideStyle = () => {
-  if (logseq.settings!.booleanFunction === false) return;
+  if (logseq.settings!.booleanFunction === false) return
 
-  let printCSS = "";
-  let settingsCSS = "";
+  let printCSS = ""
+  let settingsCSS = ""
 
   //サンプル
   // &[data-refs-self*='"@page']>.flex.flex-row.pr-2 .bullet-container .bullet:before {
@@ -156,105 +188,147 @@ export const provideStyle = () => {
   //設定に合わせて生成するCSS
 
   //12個複製する
-  let tagsListFull: string[] = [];
+  let tagsListFull: string[] = []
   for (let i = 1; i <= 12; i++) {
-    const count = ("0" + i).slice(-2);
-    const { outCSS, outTagsList } = eachCreateCSS(count);
-    tagsListFull = tagsListFull.concat(outTagsList);
-    printCSS += outCSS;
+    const count = ("0" + i).slice(-2)
+    const { outCSS, outTagsList } = eachCreateCSS(count)
+    tagsListFull = tagsListFull.concat(outTagsList)
+    printCSS += outCSS
 
-    settingsCSS += eachCreateSettingCSS(count);
+    settingsCSS += eachCreateSettingCSS(count)
   }
 
   if (printCSS !== "" && tagsListFull.length > 0) {
-    //動的CSS
-    logseq.provideStyle({
-      key: 'bullet-point-custom-icon', style: `
+    const style = `
     ${settingsCSS}
-    body>div#root>div>main>div#app-container {
+    body>#root>div>main>#app-container {
       ${logseq.settings!.booleanAtMarkTagHidden === true ? `
-      /*Optional: hide tags with @*/
-      & a.tag[data-ref*="@"] {
-          display: none;
-      }
-  `: ""}
-      & div.ls-block {
-          &:is(${[...new Set(tagsListFull)].map((tag) => `${logseq.settings!.booleanHierarchyParentTag === false ? `:not([data-refs-self*='${tag}/' i])` : ""}[data-refs-self*='"${tag}"' i]`).join(",")}) {
-            ${logseq.settings!.booleanIconLarge === "medium" ?
+        /* Optional: hide tags with @ */
+        & a.tag[data-ref*="@"] {
+            display: none;
+        }
+       `: ""}
+        & div.ls-block {
+
+            ${logseqVersionMd === true ? (`
+            &:is(${[...new Set(tagsListFull)].map((tag) => `${logseq.settings!.booleanHierarchyParentTag === false ? `:not([data-refs-self*='${tag}/' i])` : ""}[data-refs-self*='"${tag}"' i]`).join(",\n            ")}) {
+              ${logseq.settings!.booleanIconLarge === "medium" ?
           "&>.flex.flex-row.pr-2 .bullet-container .bullet:before {font-size: 1.2em;top: -10px!important;left: -7px!important;}"
-          : logseq.settings!.booleanIconLarge === "large" ? "&>.flex.flex-row.pr-2 .bullet-container .bullet:before {font-size: 1.5em;top:-13px!important;left:-8.5px!important;}"
-            : ""}  
-            ${CSS}
-          }
-          ${printCSS}
-      }
+          : logseq.settings!.booleanIconLarge === "large" ?
+            "&>.flex.flex-row.pr-2 .bullet-container .bullet:before {font-size: 1.5em;top:-13px!important;left:-8.5px!important;}"
+            : ""}
+            }  
+              ${CSS}
+            }`) : // DBモデル
+        (`${[...new Set(tagsListFull)].map((tag) => `&:has(>div.block-main-container>div.block-control-wrap+div>div.block-main-content>div>div.block-content-or-editor-wrap>div.block-content-or-editor-inner>div>div.block-content-wrapper>div.block-content>div.block-content-inner>div.block-head-wrap>div>span.block-title-wrap>span>span.preview-ref-link>a[data-ref='${tag}' i])`).join(",\n            ")} {
+                  &>div>div.block-control-wrap {
+                      &:has(span.bullet-closed)>a.block-control>span.control-hide {
+                          display: unset;
+                      }
+                      &>a.bullet-link-wrap>span.bullet-container {
+                          &>span.bullet:before {
+                              ${logseq.settings!.booleanIconLarge === "medium" ? "font-size: 1.2em;" : "font-size: 1.5em;"};
+                              font-family: 'tabler-icons';
+                              position: relative;
+                              top: -9px;
+                              left: -7.8px;
+                          }
+                          &.bullet-closed {
+                              opacity: 0.5;
+                              font-size: 0.5em;
+                          }
+                      }
+                  }
+            }`)}
+${printCSS}
+        }
     }
     `
-    });
-  } else {
-    //固定CSSのみ
-    if (logseq.settings!.booleanAtMarkTagHidden === true) logseq.provideStyle({
-      key: 'bullet-point-custom-icon', style: `
-    body>div#root>div>main>div#app-container {
-      /*Optional: hide tags with @*/
-      & a.tag[data-ref*="@"] {
-          display: none;
-      }
-    }
-    ` });
-  }
-};
+    //動的CSS
+    logseq.provideStyle({
+      key: 'bullet-point-custom-icon', style
+    })
 
+    // console.log(style)
 
-const eachCreateSettingCSS = (count: string) => {
-  return `
-    div[data-key="icon${count}"]:has(+ div[data-key="colorBoolean${count}"] > label > input[type="checkbox"]:checked) > label {
-      color: ${logseq.settings![`color${count}`]};
+  } else
+    if (logseq.settings!.booleanAtMarkTagHidden === true) //固定CSSのみ
+      logseq.provideStyle({
+        key: 'bullet-point-custom-icon', style: `
+    body>#root>div>main>#app-container {
+        /*Optional: hide tags with @*/
+        & a.tag[data-ref*="@"] {
+            display: none;
+        }
     }
-  `
+    ` })
 }
 
+
+const eachCreateSettingCSS = (count: string) => `
+    div[data-key="icon${count}"]:has(+ div[data-key="colorBoolean${count}"] > label > input[type="checkbox"]:checked) > label {
+        color: ${logseq.settings![`color${count}`]};
+    }
+  `
+
 const eachCreateCSS = (count: string, iconOff?: boolean) => {
-  let outCSS = "";
+  let outCSS = ""
   //各行が空でないこと
-  const outTagsList: string[] = (logseq.settings![`tagsList${count}`].includes("\n") ? logseq.settings![`tagsList${count}`].split("\n") : [logseq.settings![`tagsList${count}`]]).filter((tag) => tag !== "");
+  const settingsTagsList = logseq.settings![`tagsList${count}`] as string
+  const outTagsList: string[] = (settingsTagsList.includes("\n") ?
+    settingsTagsList.split("\n")
+    : [settingsTagsList]).filter((tag) => tag !== "")
   if ((iconOff === false && logseq.settings![`icon${count}`] !== "") || outTagsList.length > 0) {
-    outCSS += `&:is(${outTagsList.map((tag) => `${logseq.settings!.booleanHierarchyParentTag === false ? `:not([data-refs-self*='${tag}/' i])` : ""}[data-refs-self*='"${tag}"' i]`).join(",")})>.flex.flex-row.pr-2 .bullet-container .bullet:before {
+    outCSS += logseqVersionMd === true ?
+      `
+      &:is(${outTagsList.map((tag) => `${logseq.settings!.booleanHierarchyParentTag === false ?
+        `:not([data-refs-self*='${tag}/' i])`
+        : ""}[data-refs-self*='"${tag}"' i]`).join(",")})>.flex.flex-row.pr-2 .bullet-container .bullet:before {
         content: "${tableIconNonUnicode(count)}" !important;
-        ${logseq.settings![`colorBoolean${count}`] === true ? `color: ${logseq.settings![`color${count}`]};` : ""}
+        ${logseq.settings![`colorBoolean${count}`] === true ?
+        `color: ${logseq.settings![`color${count}`]};`
+        : ""}
       }
-      `;
+      ` : // DBモデル
+      `${outTagsList.map((tag) => `
+            &:has(>div.block-main-container>div.block-control-wrap+div>div.block-main-content>div>div.block-content-or-editor-wrap>div.block-content-or-editor-inner>div>div.block-content-wrapper>div.block-content>div.block-content-inner>div.block-head-wrap>div>span.block-title-wrap>span>span.preview-ref-link>a[data-ref='${tag}' i])`).join("        ,\n")} {
+                &>div>div.block-control-wrap>a.bullet-link-wrap>span.bullet-container>span.bullet:before {
+                    content: "${tableIconNonUnicode(count)}" !important;
+                    ${logseq.settings![`colorBoolean${count}`] === true ? `color: ${logseq.settings![`color${count}`]};` : ""}
+                }
+            }
+      `
   }
-  return { outCSS, outTagsList };
-};
+  return { outCSS, outTagsList }
+}
 
 
-export const openPopupSettingsChanged = () => openPopupFromToolbar();
+export const openPopupSettingsChanged = () => openPopupFromToolbar()
 
 const openPopupFromToolbar = () => {
-  let printCurrentSettings = "";
+  let printCurrentSettings = ""
   //現在の設定を表示
   for (let i = 1; i < 13; i++) {
     //二桁にしたい
-    const count = ("0" + i).slice(-2);
-    const { outCSS, outTagsList } = eachCreateCSS(count, true);
+    const count = ("0" + i).slice(-2)
+    const { outCSS, outTagsList } = eachCreateCSS(count, true)
     if (outTagsList.length > 0) {
       //タグを一つずつ表示し、<button>でクリックするとそのタグをコピーする。,で区切る
-      const tagsList = outTagsList.map((tag) => `<button class="button" id="customBulletIconToolbar--${count}${tag}" title="${t("Click here to insert a tag at the end of the current block.")}">#${tag}</button>`);
+      const tagsList = outTagsList.map((tag) => `<button class="button" id="customBulletIconToolbar--${count}${tag}" title="${t("Click here to insert a tag at the end of the current block.")}">#${tag}</button>`)
 
-      const icon = tablerIconGetUnicode(count);
+      const icon = tablerIconGetUnicode(count)
       printCurrentSettings += `
           <div><small>${t("Icon")} ${count}:</small><span class="tabler-icon"${logseq.settings![`colorBoolean${count}`] === true ? ` style="color:${logseq.settings![`color${count}`]}"` : ""}>${icon}</span>
           <span style="display:unset">${tagsList}</span></div>
           <hr/>
-          `;
+          `
       //outTagsListに${count}を付けて、タグを一つずつ表示し、<button>でクリックするとそのタグをコピーする。,で区切る
       outTagsList.forEach((tag) => {
         setTimeout(() => {
-          const copyTag = parent.document.getElementById(`customBulletIconToolbar--${count}${tag}`) as HTMLButtonElement | null;
-          if (copyTag) copyTag.addEventListener("click", () => copyEvent(tag));
-        }, 120);
-      });
+          const copyTag = parent.document.getElementById(`customBulletIconToolbar--${count}${tag}`) as HTMLButtonElement | null
+          if (copyTag) copyTag.addEventListener("click", () => copyEvent(tag))
+        }, 120)
+      })
     }
   }
 
@@ -290,13 +364,34 @@ const openPopupFromToolbar = () => {
         </div>
         </div>
         `,
-  });
+  })
   setTimeout(() => {
     //設定画面を開くボタンをクリックしたら、設定画面を開く
-    const showSettingsUI = parent.document.getElementById("bullet-point-custom-icon--showSettingsUI") as HTMLButtonElement | null;
-    if (showSettingsUI) showSettingsUI.addEventListener("click", () => logseq.showSettingsUI(), { once: true });
-  }, 50);
-};
+    const showSettingsUI = parent.document.getElementById("bullet-point-custom-icon--showSettingsUI") as HTMLButtonElement | null
+    if (showSettingsUI) showSettingsUI.addEventListener("click", () => logseq.showSettingsUI(), { once: true })
+  }, 50)
+}
 
 
-logseq.ready(main).catch(console.error);
+// MDモデルかどうかのチェック DBモデルはfalse
+const checkLogseqVersion = async (): Promise<boolean> => {
+  const logseqInfo = (await logseq.App.getInfo("version")) as AppInfo | any
+  //  0.11.0もしくは0.11.0-alpha+nightly.20250427のような形式なので、先頭の3つの数値(1桁、2桁、2桁)を正規表現で取得する
+  const version = logseqInfo.match(/(\d+)\.(\d+)\.(\d+)/)
+  if (version) {
+    logseqVersion = version[0] //バージョンを取得
+    // console.log("logseq version: ", logseqVersion)
+
+    // もし バージョンが0.10.*系やそれ以下ならば、logseqVersionMdをtrueにする
+    if (logseqVersion.match(/0\.([0-9]|10)\.\d+/)) {
+      logseqVersionMd = true
+      // console.log("logseq version is 0.10.* or lower")
+      return true
+    } else logseqVersionMd = false
+  } else logseqVersion = "0.0.0"
+  return false
+}
+// DBグラフかどうかのチェック
+const checkLogseqDbGraph = async (): Promise<boolean> => await (logseq.App as any).checkCurrentIsDbGraph() as boolean | false || false
+
+logseq.ready(main).catch(console.error)
--- src/lib.ts
+++ src/lib.ts
@@ -1,44 +1,44 @@
-import { BlockEntity } from '@logseq/libs/dist/LSPlugin.user';
-import { t } from 'logseq-l10n';
+import { BlockEntity } from '@logseq/libs/dist/LSPlugin.user'
+import { t } from 'logseq-l10n'
 
 export const removeProvideStyle = (className: string) => {
     const doc = parent.document.head.querySelector(
         `style[data-injected-style^="${className}"]`
-    ) as HTMLStyleElement | null;
-    if (doc) doc.remove();
-};
+    ) as HTMLStyleElement | null
+    if (doc) doc.remove()
+}
 
-export const tableIconNonUnicode = (count: string) => logseq.settings![`icon${count}`].length === 4 ? "\\" + logseq.settings![`icon${count}`] : logseq.settings![`icon${count}`];
+export const tableIconNonUnicode = (count: string) => (logseq.settings![`icon${count}`] as string).length === 4 ? "\\" + logseq.settings![`icon${count}`] : logseq.settings![`icon${count}`]
 
-export const tablerIconGetUnicode = (count: string) => logseq.settings![`icon${count}`] ?
+export const tablerIconGetUnicode = (count: string) => (logseq.settings![`icon${count}`] as string) ?
     //4桁の文字列であれば、&#xを付けて、;を付ける
-    logseq.settings![`icon${count}`].length === 4 ? `&#x${logseq.settings![`icon${count}`] as string};` :
+    (logseq.settings![`icon${count}`] as string).length === 4 ? `&#x${logseq.settings![`icon${count}`] as string};` :
         //5桁の文字列であれば、\を削除して、&#xを付けて、;を付ける
-        logseq.settings![`icon${count}`].length === 5 ?
+        (logseq.settings![`icon${count}`] as string).length === 5 ?
             `&#x${(logseq.settings![`icon${count}`] as string).replace("\\", "") as string};` : logseq.settings![`icon${count}`]
-    : "---";
+    : "---"
 
 export const copyEvent = async (tag: string) => {
     //現在のブロックを取得
-    const currentBlock = await logseq.Editor.getCurrentBlock() as BlockEntity | null;
+    const currentBlock = await logseq.Editor.getCurrentBlock() as BlockEntity | null
     if (currentBlock) {
-        const ele = parent.document.querySelector(`div#root>div>main>div#app-container div[blockid="${currentBlock.uuid}"]`) as HTMLDivElement | null;
+        const ele = parent.document.querySelector(`div#root>div>main>div#app-container div:is([blockid="${currentBlock.uuid}"],id="${currentBlock.uuid}"])`) as HTMLDivElement | null
         if (ele) {
-            ele.style.outline = "3px solid var(--ls-border-color)";
-            setTimeout(() => ele.style.outline = "unset", 6000);
+            ele.style.outline = "3px solid var(--ls-border-color)"
+            setTimeout(() => ele.style.outline = "unset", 6000)
         }
         //現在のブロックの最後にタグを追加
         //tagに空白が含まれていたら、[[ ]]で囲む
-        if (tag.includes(" ")) tag = "[[" + tag + "]]";
+        if (tag.includes(" ")) tag = "[[" + tag + "]]"
         //contentの中に、\nが含まれている場合、一つ目の\nの前に、tagを挿入する
-        let content = currentBlock.content;
-        if (content.includes("\n")) content = content.replace("\n", " #" + tag + "\n");
-        else content = content + " #" + tag;
-        await logseq.Editor.updateBlock(currentBlock.uuid, content, currentBlock.properties);
-        logseq.UI.showMsg(t("Insert at editing block: #") + tag + ".", "info");
-        logseq.Editor.editBlock(currentBlock.uuid);
+        let content = currentBlock.content
+        if (content.includes("\n")) content = content.replace("\n", " #" + tag + "\n")
+        else content = content + " #" + tag
+        await logseq.Editor.updateBlock(currentBlock.uuid, content, currentBlock.properties)
+        logseq.UI.showMsg(t("Insert at editing block: #") + tag + ".", "info")
+        logseq.Editor.editBlock(currentBlock.uuid)
     } else {
         //ブロックが選択されていない場合
-        logseq.UI.showMsg(t("No block selected."), "warning");
+        logseq.UI.showMsg(t("No block selected."), "warning")
     }
-};
+}
